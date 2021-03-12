8bf8e791d56c1bf355fb24db4d0ae771ba32c8a9
sujit0892 <sujitkumar0892@gmail.com>
2021-03-12 15:00:54 +0530

    Junit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void factOne() {
+        assertEquals("testcase 1", 120, calculator.fact(5));
+        ;
+    }
+
+    @Test
+    public void factTwo() {
+        assertFalse("testcase 2", 120 == calculator.fact(6));
+        ;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void factNegatve() {
+        long f = -1;
+        calculator.fact(-1);
+    }
+
+    @Test
+    public void power() {
+        assertTrue("testcase 3", 16 == calculator.power(4, 2));
+        ;
+    }
+
+    @Test
+    public void log()
+    {
+        assertTrue("testcase 4", 0.6931471805599453 == calculator.log(2));
+        ;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void logNegatve() {
+        long f = -1;
+        calculator.log(-1);
+    }
+
+    @Test
+    public void sqrt()
+    {
+        assertTrue("testcase 4", 4 == calculator.sqrt(16));
+        ;
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sqrtNegatve() {
+        long f = -1;
+        calculator.sqrt(-1);
+    }
+
+
+
+
+
+}
